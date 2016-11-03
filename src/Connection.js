3d108e039cbb6f44425956c272ab7d2f6bbd2e61
Mauricio D Angelo Fernandes <maudfer@gmail.com>
2016-11-03 08:33:07 -0200

    Cria um arquivo separado para gerenciar a conexão
    O arquivo Connection.js será responsável por abrir e fechar conexões, bem como expor o acesso à plataforma e o estado atual da conexão.

---
+++ src/Connection.js
@@ -0,0 +1,47 @@
+
+/** @module Connection */
+
+function Connection() {
+
+}
+
+function reset() {
+
+}
+
+function getState() {
+
+}
+
+function connect(){
+
+}
+
+function disconnect() {
+
+}
+
+/*function findPlat() {
+	console.log('batata1 findPlat()');
+	port = null;
+	btConnecting();
+	var found = false;
+	browserserialport.list(function(err, ports) {
+		var counter = 0;
+		console.log('batata list');
+		ports.forEach(function(port) {
+			counter++;
+			console.log('batata c++ forEach');
+			if (port.manufacturer.indexOf('Arduino') !== -1 && !found) {
+				console.log('batata achou e con' + port.comName);
+				connect(port.comName);
+				console.log(port.comName);
+				found = true;
+			}
+			if (counter === ports.length && !found) {
+				btERR('Porta não encontrada!');
+			}
+		});
+	});
+}*/
+
